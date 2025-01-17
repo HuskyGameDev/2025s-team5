c508b5663b73f3356f672d700e3ce7c0bb70058b
maxlaskaris <maxlaskaris@gmail.com>
2025-01-16 22:24:35 -0500

    Created Shell and Upgrade Objects

---
+++ tropical-tanks/Objects/Upgrade/upgrade.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+# Placeholder int 
+# REPLACE WITH ENUM
+@export var Upgrade : int
+
+# Passed when an object enters the collection area
+func _on_collection_area_3d_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
