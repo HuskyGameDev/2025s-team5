f0ae82dc57953a84f68dd273024014afbed93115
Jake Rundle <jrundle1618@gmail.com>
2025-01-26 19:34:45 -0500

    Added Shell Parameters

--- tropical-tanks/Objects/Shell/shell.gd
+++ tropical-tanks/Objects/Shell/shell.gd
@@ -7,7 +7,11 @@ const EXPLOSION = preload("res://Objects/Explosion/explosion.tscn")
 var velocity : Vector3 = Vector3(0,0,0)
 var gravity : Vector3 = ProjectSettings.get_setting("physics/3d/default_gravity_vector") * ProjectSettings.get_setting("physics/3d/default_gravity")
 var mass : float = 1.0
-
+var ap_power : float = 0.0
+var expl_power : float = 1.0
+var evap : float = 1.0
+var bounce : float = 0.0
+var fuse = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
