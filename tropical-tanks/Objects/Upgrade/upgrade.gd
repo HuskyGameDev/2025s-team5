9f04cb4c10dcb2440e48ecf1c4d5bcf74ab20f78
Jake Rundle <jrundle1618@gmail.com>
2025-03-04 20:13:01 -0500

    upgrade collection fix
    Upgrades can only be picked up by the player now

--- tropical-tanks/Objects/Upgrade/upgrade.gd
+++ tropical-tanks/Objects/Upgrade/upgrade.gd
@@ -4,7 +4,12 @@ extends Node3D
 # REPLACE WITH ENUM
 @export var Upgrade : int
 
+@onready var player_tank: Node3D = $"../PlayerRoot"
+
+
 # Passed when an object enters the collection area
 func _on_collection_area_3d_body_entered(body: Node3D) -> void:
-	print("Upgrade collected!")
-	queue_free()
+	if(!body.is_in_group("Enemy")):
+		
+		print("Upgrade collected!")
+		queue_free()
