438f6cf54eea6a0eef0319810699c191e114955d
Jake Rundle <jrundle1618@gmail.com>
2025-03-13 11:46:28 -0400

    Update Upgrade.gd

--- tropical-tanks/Classes/Upgrade.gd
+++ tropical-tanks/Classes/Upgrade.gd
@@ -1,2 +1,9 @@
 extends Resource
 class_name Upgrade
+
+@export var double_barrel : int = 0 :
+	set(value):
+		double_barrel = value
+@export var split_barrel : int = 0 :
+	set(value):
+		split_barrel = value
