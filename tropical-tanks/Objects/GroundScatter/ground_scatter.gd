b5a00025b7a4c4715c58daf72e4ce7a3de4bd7c8
maxlaskaris <maxlaskaris@gmail.com>
2025-03-15 16:42:09 -0400

    Removed Placeholder Materials

--- tropical-tanks/Objects/GroundScatter/ground_scatter.gd
+++ tropical-tanks/Objects/GroundScatter/ground_scatter.gd
@@ -1,25 +1,10 @@
 @tool
 extends Node3D
 
-@export var MODEL : PackedScene
 
-		
-		
-var model_scene : Node3D
-	#set(value):
-		#model_scene = value
-		#for child in $MeshRoot.get_children():
-			#child.queue_free()
-		#$MeshRoot.add_child(model_scene)
-		
 @export var is_collidable : bool = false
-
 
 func death():
 	queue_free()
 
-func _ready() -> void:
-	model_scene = MODEL.instantiate()
-	rotation.y = randf() * TAU
-	$MeshRoot.add_child(model_scene)
-	
+@export var mesh : MeshInstance3D
