54452c4121869ce0a8f67db19686c1241e11b549
Mat Carvalho Valverde <mccarval@mtu.edu>
2025-03-20 01:59:54 -0400

    Many updates to settings menu
    New background and layout, implementation of fps controls, setup for other functions such as brightness, sensitivity, etc

--- tropical-tanks/Scenes/MainMenu/settings.gd
+++ tropical-tanks/Scenes/MainMenu/settings.gd
@@ -27,18 +27,34 @@ func _on_back_pressed() -> void:
 func _on_option_button_item_selected(index: int) -> void:
 	match index:
 		0:
-			pass
+			Engine.max_fps = 0
 		1:
-			pass
+			Engine.max_fps = 144
 		2:
-			pass
+			Engine.max_fps = 60
 		3:
-			pass
+			Engine.max_fps = 30
 
 
 func _on_button_mappings_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_h_slider_changed(value: float) -> void:
-	pass # Replace with function body.
+func _process(delta: float) -> void:
+	$currentFPS.text = "FPS: " + str(Engine.get_frames_per_second())
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	if (toggled_on == true):
+		$currentFPS.hide()
+	else:
+		$currentFPS.show()
+
+
+var MOUSE_SENSITIVITY = 1
+func _on_sensitivity_changed(value) -> void:
+	MOUSE_SENSITIVITY = value
+
+
+func _on_brightness_changed(value) -> void:
+	EnvironmentScene.Environment.adjustment_brightness = value
