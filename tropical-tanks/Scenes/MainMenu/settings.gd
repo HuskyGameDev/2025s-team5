f274899105dc3d4e8f9756bdf3581faa37e18e24
Mat Carvalho Valverde <mccarval@mtu.edu>
2025-03-23 19:18:17 -0400

    Settings scene is finally kinda good fr

--- tropical-tanks/Scenes/MainMenu/settings.gd
+++ tropical-tanks/Scenes/MainMenu/settings.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var fps = $MarginContainer/PanelContainer/VBoxContainer/Top/currentFPS
+
+
+func _process(delta: float) -> void:
+	fps.text = "FPS: " + str(int(Engine.get_frames_per_second()))
+
+
 func _on_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0, value)
 	
@@ -27,18 +34,28 @@ func _on_back_pressed() -> void:
 func _on_option_button_item_selected(index: int) -> void:
 	match index:
 		0:
-			pass
+			Engine.max_fps = 0
 		1:
-			pass
+			Engine.max_fps = 144
 		2:
-			pass
+			Engine.max_fps = 60
 		3:
-			pass
+			Engine.max_fps = 30
 
 
 func _on_button_mappings_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_h_slider_changed(value: float) -> void:
-	pass # Replace with function body.
+func _on_fps_show_toggled(toggled_on: bool) -> void:
+	if (toggled_on == true):
+		fps.hide()
+	else:
+		fps.show()
+
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if (toggled_on):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
