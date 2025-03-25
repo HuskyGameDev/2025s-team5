4b07522d770175f4ea3b10b17c78c645b5d186c7
maxlaskaris <maxlaskaris@gmail.com>
2025-03-25 13:32:13 -0400

    Spawn Explosion Resource Configuration

--- tropical-tanks/Resources/ResourceClasses/SpawnExplosion.gd
+++ tropical-tanks/Resources/ResourceClasses/SpawnExplosion.gd
@@ -1,11 +1,11 @@
 extends Resource
 class_name SpawnExplosion
 
-@export var explosion_power : float = 1.0
+@export var explosion_power : Vector2 = Vector2(1.0,1.0)
 const EXPLOSION = preload("res://Components/Explosion/explosion.tscn")
 func trigger_effect(node : Node3D) -> void:
 	#assert(OBJECT, "Spawn Object requires an object")
 	var explosion : Explosion = EXPLOSION.instantiate()
-	explosion.explosion_power = explosion_power
+	explosion.explosion_power = randf_range(explosion_power.x,explosion_power.y)
 	explosion.position = node.global_position
 	node.get_tree().root.add_child(explosion)
