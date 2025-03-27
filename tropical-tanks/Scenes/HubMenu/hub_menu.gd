21610ab55a35771e7c17214fc14c3766b112b7f5
Mat Carvalho Valverde <mccarval@mtu.edu>
2025-03-27 10:56:22 -0400

    Starting the area selector pop up, not sure where this color map came from

--- tropical-tanks/Scenes/HubMenu/hub_menu.gd
+++ tropical-tanks/Scenes/HubMenu/hub_menu.gd
@@ -2,7 +2,13 @@ extends Node3D
 
 var area = "res://Scenes/Areas/area_tropical.tscn"
 # Called when the node enters the scene tree for the first time.
-
+@onready var canvasLayer = $CanvasLayer
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file(area)
+
+
+var AREASELECTOR_SCENE = preload("res://Scenes/HubMenu/AreaSelector.tscn")
+func _on_area_select_pressed() -> void:
+	var scene = AREASELECTOR_SCENE.instantiate()
+	canvasLayer.add_child(scene)
